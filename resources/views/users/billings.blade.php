ab3ea8c2c1e3239fcc2268b1b105def3e5eb2f6d
Patrick FARRUGIA <farrugiapatrick@free.fr>
2019-10-02 22:36:06 +0200

    Feature (Billing) : Add purchase date in table

--- resources/views/users/billings.blade.php
+++ resources/views/users/billings.blade.php
@@ -8,10 +8,11 @@
     <div class="container">
         <h1>{{ __('Your :count billings', ['count' => count($billings)]) }}</h1>
 
-        <table class="table table-bordered">
+        <table class="table table-bordered table-hover">
             <thead>
             <tr>
                 <th>#</th>
+                <th>{{ __('Purchase date') }}</th>
                 <th>{{ __('Total amount including taxes') }}</th>
                 <th>{{ __('Number of items') }}</th>
             </tr>
@@ -23,6 +24,7 @@
                     <td>
                         <a href="{{ route('billing.export', ['billing' => $billing->number]) }}">{{ $billing->number }}</a>
                     </td>
+                    <td>{{ $billing->updated_at->format('Y-m-d') }}</td>
                     <td>{{ $billing->formatted_total_amount_including_taxes }}</td>
                     <td> {{ $billing->items_count }}</td>
                 </tr>
